523d599076e89dd40e317031d86eaa49ea654f18
calculatorLesha <loko.sagaydack@gmail.com>
2022-09-24 12:37:59 +0300

    Initial Commit

---
+++ Calculator2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator2.0
+//
+//  Created by Алина Сагайдак on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
